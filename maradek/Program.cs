ca11e2ab0826efa60a576fbc55a39439ab097d54
tanulo <tanulo@TANULOGEP-711>
2026-09-22 09:42:40 +0200

    Add project files.

---
+++ maradek/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maradek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("kérek egy pénz összeget: ");
+            int osszeg = int.Parse(Console.ReadLine());
+            Console.WriteLine($"A kerekitett összeg: {osszeg - osszeg%5}ft");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ matek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osztas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("kérek egy számot:");
+            int szam = int.Parse(Console.ReadLine());
+            Console.WriteLine($"a szám köbe: {Math.Pow(szam,(double)1/3)}");
+
+
+            Console.ReadKey();  
+        }
+    }
+}
